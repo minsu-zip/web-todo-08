7710484dfb8c384c03cce08b8bb483def4da7617
gywlsp <gywls517@gmail.com>
2022-07-13 15:13:24 +0900

    feat: :sparkles: ConfirmModal 컴포넌트 추가

---
+++ src/frontend/components/ConfirmModal.js
@@ -0,0 +1,38 @@
+export default function ConfirmModal({ $target, initialState }) {
+  this.$element = document.createElement('div')
+  this.$element.classList.add('confirm-modal-overlay')
+  this.$element.classList.add('closed')
+  $target.appendChild(this.$element)
+
+  this.state = initialState
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    this.render()
+  }
+
+  this.render = () => {
+    this.$element.innerHTML = `
+        <div class="confirm-modal">
+            <p class="confirm-message">${this.state?.message}</p>
+            <div class="confirm-action-btns">
+                <button class="confirm-cancelBtn">취소</button>
+                <button class="confirm-submitBtn">${this.state?.submitText}</button>
+            </div>
+        </div>
+    `
+    document
+      .querySelector('.confirm-submitBtn')
+      .addEventListener('click', this.state.onSubmit)
+  }
+
+  this.open = () => {
+    this.$element.classList.remove('closed')
+  }
+
+  this.close = () => {
+    this.$element.classList.add('closed')
+  }
+
+  this.render()
+}
