605f41901233c9ee84b8e19df67ec8514713115b
gywlsp <gywls517@gmail.com>
2022-07-14 11:15:17 +0900

    refactor: :recycle: state 변경에 따라 removeTodo 메서드 동작하도록 리팩토링

--- src/frontend/components/todo/Container.js
+++ src/frontend/components/todo/Container.js
@@ -28,21 +28,14 @@ export default function TodoContainer({ $target }) {
     })
   }
 
-  this.getTodoByStatus = () => {
-    return this.state.todos.reduce(
-      (acc, curr) => {
-        acc[todoColumnData.findIndex((v) => v.status === curr.status)].push(
-          curr
-        )
-        return acc
-      },
-      [[], [], []]
-    )
-  }
-
   this.removeTodo = (id) => {
+    const newTodosByStatus = this.state.todosByStatus.map((todos) => {
+      const todoIndex = todos.findIndex((todo) => todo.id === id)
+      if (todoIndex === -1) return [...todos]
+      return todos.filter((todo) => todo.id !== id)
+    })
     this.setState({
-      todos: this.state.todos.filter((todo) => todo.id !== id),
+      todosByStatus: newTodosByStatus,
     })
   }
 
@@ -94,34 +87,34 @@ export default function TodoContainer({ $target }) {
     this.setState({
       todosByStatus: [
         [
-        {
-          id: 1,
-          status: 'todo',
-          title: '컴포넌트 구현',
-          description: '화면에 필요한 컴포넌트들을 생성합니다.',
-        },
-        {
-          id: 2,
-          status: 'todo',
-          title: 'API 구현',
-          description: '웹 어플리케이션에 필요한 API를 구현합니다.',
-        },
+          {
+            id: 1,
+            status: 'todo',
+            title: '컴포넌트 구현',
+            description: '화면에 필요한 컴포넌트들을 생성합니다.',
+          },
+          {
+            id: 2,
+            status: 'todo',
+            title: 'API 구현',
+            description: '웹 어플리케이션에 필요한 API를 구현합니다.',
+          },
         ],
         [
-        {
-          id: 3,
-          status: 'inprogress',
-          title: '컴포넌트 구조 설계',
-          description: '컴포넌트 구조를 설계합니다.',
-        },
+          {
+            id: 3,
+            status: 'inprogress',
+            title: '컴포넌트 구조 설계',
+            description: '컴포넌트 구조를 설계합니다.',
+          },
         ],
         [
-        {
-          id: 4,
-          status: 'done',
-          title: '기본 스타일 추가',
-          description: '기본 스타일 파일을 추가합니다.',
-        },
+          {
+            id: 4,
+            status: 'done',
+            title: '기본 스타일 추가',
+            description: '기본 스타일 파일을 추가합니다.',
+          },
         ],
       ],
     })
