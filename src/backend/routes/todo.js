5ea3174034e836f1d5201acce11cb5411e7eb75c
gywlsp <gywls517@gmail.com>
2022-07-14 20:44:42 +0900

    feat: :sparkles: TodoRouter에 delete 핸들러 추가

--- src/backend/routes/todo.js
+++ src/backend/routes/todo.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router.get('/', todoController.getTodos)
 router.post('/', todoController.postTodo)
+router.delete('/:id', todoController.deleteTodo)
 router.patch('/:id', todoController.updateTodo)
 
 module.exports = router
