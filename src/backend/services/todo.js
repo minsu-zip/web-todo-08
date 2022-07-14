636d6ff5b6bf6a67cc5d21850137d8194ad247dc
gywlsp <gywls517@gmail.com>
2022-07-14 15:33:24 +0900

    refactor: :recycle: get todo list api 응답 형식 수정

--- src/backend/services/todo.js
+++ src/backend/services/todo.js
@@ -1,10 +1,22 @@
 const dbPool = require('../config/db')
 
+const todoStatus = ['todo', 'inprogress', 'done']
+
 class TodoService {
   getTodos(resCallback) {
     const sql = 'select * from todo;'
     dbPool.query(sql, (err, todos) => {
-      resCallback(todos)
+      const todosByStatus = todos.reduce(
+        (acc, todo) => {
+          {
+            acc[todoStatus.indexOf(todo.status)].push(todo)
+            return acc
+          }
+        },
+        [[], [], []]
+      )
+      todosByStatus.forEach((todos) => todos.sort())
+      resCallback(todosByStatus)
     })
   }
 }
