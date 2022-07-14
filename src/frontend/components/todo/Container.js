2fbc7b2234c7f5856e2762673e24c57578bc2ef5
gywlsp <gywls517@gmail.com>
2022-07-14 11:13:46 +0900

    refactor: :recycle: todos state 형식, 이름 변경

--- src/frontend/components/todo/Container.js
+++ src/frontend/components/todo/Container.js
@@ -17,12 +17,12 @@ export default function TodoContainer({ $target }) {
   this.$element.appendChild(this.$dragPoint)
 
   this.state = {
-    todos: [],
+    todosByStatus: [[], [], []],
   }
 
   this.setState = (nextState) => {
     this.state = nextState
-    this.getTodoByStatus().forEach((todos, i) => {
+    this.state.todosByStatus.forEach((todos, i) => {
       const { status, label } = todoColumnData[i]
       todoColumns[i].setState({ status, title: label, todos })
     })
@@ -68,7 +68,7 @@ export default function TodoContainer({ $target }) {
     })
   }
 
-  const todoColumns = this.getTodoByStatus().map((todos, i) => {
+  const todoColumns = this.state.todosByStatus.map((todos, i) => {
     const { status, label } = todoColumnData[i]
     return new TodoColumn({
       $target: this.$element,
@@ -86,7 +86,8 @@ export default function TodoContainer({ $target }) {
   this.init = () => {
     // data fetch
     this.setState({
-      todos: [
+      todosByStatus: [
+        [
         {
           id: 1,
           status: 'todo',
@@ -99,18 +100,23 @@ export default function TodoContainer({ $target }) {
           title: 'API 구현',
           description: '웹 어플리케이션에 필요한 API를 구현합니다.',
         },
+        ],
+        [
         {
           id: 3,
           status: 'inprogress',
           title: '컴포넌트 구조 설계',
           description: '컴포넌트 구조를 설계합니다.',
         },
+        ],
+        [
         {
           id: 4,
           status: 'done',
           title: '기본 스타일 추가',
           description: '기본 스타일 파일을 추가합니다.',
         },
+        ],
       ],
     })
   }
