15f2e76c243cb7d2e186282c4fb95ce9a8561557
gywlsp <gywls517@gmail.com>
2022-07-14 18:04:42 +0900

    feat: :sparkles: TodoService에 updateTodo 메서드 추가

--- src/backend/services/todo.js
+++ src/backend/services/todo.js
@@ -19,6 +19,21 @@ class TodoService {
       resCallback(todosByStatus)
     })
   }
+
+  updateTodo(data, resCallback) {
+    const { id, ...rest } = data
+    const updateQuery =
+      [...Object.entries(rest)].reduce((acc, [key, value], index) => {
+        if (!value) return acc
+        return `${acc}${index ? ',' : ''} ${key}='${value}'`
+      }, `update todo set`) + ` where id = ${id};`
+    dbPool.query(updateQuery, (err) => {
+      const selectQuery = `select * from todo where id = ${id};`
+      dbPool.query(selectQuery, (err, todo) => {
+        resCallback(todo)
+      })
+    })
+  }
 }
 
 module.exports = TodoService
