e97ec192a7e6615a9c509ada909d14dd489a9c7d
gywlsp <gywls517@gmail.com>
2022-07-14 18:05:32 +0900

    feat: :sparkles: TodoRouter에 patch 핸들링 추가

--- src/backend/routes/todo.js
+++ src/backend/routes/todo.js
@@ -4,5 +4,6 @@ const todoController = require('../controllers/todo')
 const router = express.Router()
 
 router.get('/', todoController.getTodos)
+router.patch('/:id', todoController.updateTodo)
 
 module.exports = router
