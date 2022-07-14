f9c74f21254290d70912213384af3b34cea09f46
gywlsp <gywls517@gmail.com>
2022-07-14 20:45:09 +0900

    feat: :sparkles: 프론트엔드 TodoAPI에 delete 메서드 추가

--- src/frontend/api/todo.js
+++ src/frontend/api/todo.js
@@ -8,10 +8,15 @@ const patch = (id, body, resCallback) =>
 const post = (body, resCallback) =>
   API.post(`/api/todos`, body).then(resCallback)
 
+//'delete' is not allowed as a variable declaration name.
+const _delete = (id, resCallback) =>
+  API.delete(`/api/todos/${id}`).then(resCallback)
+
 const TodoAPI = {
   getList,
   patch,
   post,
+  delete: _delete,
 }
 
 export default TodoAPI
