7e4ee675f7907bbd588d28ad820f206c86ad5fa1
gywlsp <gywls517@gmail.com>
2022-07-14 18:01:49 +0900

    feat: :sparkles: TodoAPI에 patch 함수 추가

--- src/frontend/api/todo.js
+++ src/frontend/api/todo.js
@@ -2,8 +2,12 @@ import { API } from '.'
 
 const getList = (resCallback) => API.get('/api/todos').then(resCallback)
 
+const patch = (id, body, resCallback) =>
+  API.patch(`/api/todos/${id}`, body).then(resCallback)
+
 const TodoAPI = {
   getList,
+  patch,
 }
 
 export default TodoAPI
