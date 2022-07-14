cb7dcd305d4d338b7298b074430835f5fab2f8f2
gywlsp <gywls517@gmail.com>
2022-07-14 20:15:45 +0900

    feat: :sparkles: 프론트엔드 TodoAPI에 post 추가

--- src/frontend/api/todo.js
+++ src/frontend/api/todo.js
@@ -5,9 +5,13 @@ const getList = (resCallback) => API.get('/api/todos').then(resCallback)
 const patch = (id, body, resCallback) =>
   API.patch(`/api/todos/${id}`, body).then(resCallback)
 
+const post = (body, resCallback) =>
+  API.post(`/api/todos`, body).then(resCallback)
+
 const TodoAPI = {
   getList,
   patch,
+  post,
 }
 
 export default TodoAPI
